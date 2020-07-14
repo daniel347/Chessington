6e7a4c00e95f776eb5043bfc3ad93cd1cb6e191b
daniel347 <daniel.siromani@softwire.com>
2020-07-14 11:58:06 +0100

    Made pawns move

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -10,7 +10,19 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var currentPos = board.FindPiece(this);
+            var moveDir = 0;
+            
+            if (this.Player == Player.White)
+            {
+                moveDir = -1;
+            }
+            else if (this.Player == Player.Black)
+            {
+                moveDir = 1;
+            }
+            
+            return new List<Square>() {Square.At(currentPos.Row + moveDir, currentPos.Col)} ;
         }
     }
 }
