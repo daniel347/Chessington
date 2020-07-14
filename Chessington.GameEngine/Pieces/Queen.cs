9cbd8d6762562ed522619e1af429b4f7bf0161fe
daniel347 <daniel.siromani@softwire.com>
2020-07-14 13:55:06 +0100

    Queen moves diagonally and lateraly and nowhere else

--- Chessington.GameEngine/Pieces/Queen.cs
+++ Chessington.GameEngine/Pieces/Queen.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 
 namespace Chessington.GameEngine.Pieces
@@ -10,7 +11,51 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var currentPos = board.FindPiece(this);
+            var availableMoves = GetLateralMoves(board, currentPos);
+
+            availableMoves.AddRange(GetDiagonalMoves(board, currentPos));
+
+            // Remove start square, duplicates and return
+            availableMoves.RemoveAll(s => s == Square.At(4, 4));
+            return availableMoves.Distinct();
         }
+
+        private List<Square> GetLateralMoves(Board board, Square currentPos)
+        {
+            var availableMoves = new List<Square>();
+            for (var i = 0; i < 8; i++)
+                availableMoves.Add(Square.At(currentPos.Row, i));
+
+            for (var i = 0; i < 8; i++)
+                availableMoves.Add(Square.At(i, currentPos.Col));
+
+            return availableMoves;
+        }
+
+        private List<Square> GetDiagonalMoves(Board board, Square currentPos)
+        {
+            var availableMoves = new List<Square>();
+            for (var u = 0; u < GameSettings.BoardSize; u++)
+            {
+                var v1 = u - currentPos.Col + currentPos.Row;
+                var v2 = currentPos.Col + currentPos.Row - u;
+                
+                if (board.IsPositionValid(v1, u))
+                {
+                    availableMoves.Add(Square.At(v1, u));
+                }
+
+                if (board.IsPositionValid(v2, u))
+                {
+                    availableMoves.Add(Square.At(v2, u));
+                }
+            }
+
+            return availableMoves;
+        }
+
+        
+        
     }
 }
